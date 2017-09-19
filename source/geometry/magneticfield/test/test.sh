d70e672926e46918137f069765e81d2e45c13684
Dmitry Sorokin <sodi@yandex-team.ru>
2017-09-19 16:59:58 +0300

    add G4RK547FEq{1,2,3} stepper to testPropagateMagField

--- source/geometry/magneticfield/test/test.sh
+++ source/geometry/magneticfield/test/test.sh
@@ -40,7 +40,7 @@ if [[ ! -x $BINDIR/$target ]] ; then
   exit 1
 fi
 echo  "Executing $target ..."
-for n in 1 2 3 4 5 6 7 8 9 10 11 12 13  23 45 56 78 145 745
+for n in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 23 45 56 78 145 745
 do
   echo "Executing with stepper choice $n ..."
   $BINDIR/$target $n  > $target.newout$n \
